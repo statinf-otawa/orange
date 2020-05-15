d8d7fe9d051e7272c7d36f4aa6cb12bdc9f552d6
MDM <marianne.de-michiel@univ-tlse2.fr>
2020-05-15 07:53:37 +0200

    ca marche mais je ne sais pas pourquoi

--- O_Range/tMap.ml
+++ O_Range/tMap.ml
@@ -130,7 +130,7 @@ let print
   Format.fprintf fmt lastbind;
   Format.fprintf fmt last
 
-let print_only
+let print_list
     ?(first : (unit, Format.formatter, unit) format = ("[@[<hv>" : (unit, Format.formatter, unit) format))
     ?(sep : (unit, Format.formatter, unit) format = (";@ ":(unit, Format.formatter, unit) format))
     ?(last : (unit, Format.formatter, unit) format = ("@]]":(unit, Format.formatter, unit) format))
